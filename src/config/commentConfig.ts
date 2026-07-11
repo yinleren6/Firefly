df026136be89b976b8b08504e545db86255dbfea
misaka <zelda.me@outlook.com>
2026-07-11 14:54:27 +0800

    1

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -10,7 +10,7 @@ export const commentConfig: CommentConfig = {
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
-		visitorCount: true,
+		visitorCount: false,
 		// Twikoo JS 文件地址，支持 CDN 链接
 		// 中国推荐1: https://registry.npmmirror.com/twikoo/1.7.12/files/dist/twikoo.min.js
 		// 中国推荐2: https://s4.zstatic.net/npm/twikoo@1.7.13/dist/twikoo.min.js
