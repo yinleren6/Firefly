2c1c3df40163300d1140b571d324e07c0cd0bf07
misaka <zelda.me@outlook.com>
2026-07-12 02:47:07 +0800

    remove comment

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -2,11 +2,11 @@ import type { CommentConfig } from "../types/commentConfig";
 
 export const commentConfig: CommentConfig = {
 	// 评论系统类型: none, twikoo, waline, giscus, disqus, artalk，默认为none，即不启用评论系统
-	type: "twikoo",
+	type: "none",
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://twikoo.cn3.top",
+		envId: "http://twikoo.vercel.app",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
