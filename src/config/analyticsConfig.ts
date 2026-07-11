bed4c0b1dddf4ac4c52d6b6a3e30c275dfcc9444
misaka <zelda.me@outlook.com>
2026-07-12 03:59:47 +0800

    add umami

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -8,11 +8,11 @@ export const analyticsConfig: AnalyticsConfig = {
 	// Umami 统计配置
 	umamiAnalytics: {
 		// Umami Website ID
-		websiteId: "",
+		websiteId: "117d7413-1720-4e30-8efa-22670ebb6f94",
 		// Umami JS地址，支持使用自建
-		scriptUrl: "https://cloud.umami.is/script.js",
+		scriptUrl: "https://u.cn3.top/script.js",
 		// Umami 会话回放脚本地址，支持使用自建
-		replaysScriptUrl: "https://cloud.umami.is/recorder.js",
+		replaysScriptUrl: "https://u.cn3.top/recorder.js",
 		// 是否追踪出站链接
 		trackOutboundLinks: true,
 		// 是否收集浏览器性能指标
